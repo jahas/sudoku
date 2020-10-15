cd022b439ca1074546f36ed91fb6152992e2867f
adam <adam.szorcz@gmail.com>
2020-10-16 00:35:40 +0200

    read into arraybuffer of arrays

--- src/main/scala/org/nexio/SudokuAnalyzer.scala
+++ src/main/scala/org/nexio/SudokuAnalyzer.scala
@@ -1,15 +1,17 @@
 package org.nexio
 import java.io.File
 import scala.io.Source
+import scala.collection.mutable.ListBuffer
 
 object SudokuAnalyzer {
   def analyze(file: File): Boolean = {
     println("File to analyze = %s".format(file))
-    val rows = List[List[Int]]()
+    val rows = ListBuffer[Array[Int]]()
     for (line <- Source.fromFile(file.getAbsolutePath()).getLines) {
       println(line)
-      rows += List(line.split(","))
+      rows += line.split(",").map(x => if (x.length() > 0) Integer.parseInt(x) else 0)
     }
+    println(rows)
     true
   }
 }
